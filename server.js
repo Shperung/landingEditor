2e9615775e391037f0f289673db0cca7876c4d6b
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2019-05-03 13:32:33 +0300

    refactor

--- server.js
+++ server.js
@@ -6,37 +6,36 @@ const crypto = require('crypto');
 const getRegExp = require("./modules/getRegExp.js")
 const port = 3012;
 const server = http.createServer();
-const userName = 'admin';
-const userPassword = '123';
+const defaultUserName = 'admin';
+const defaultUserPassword = '123';
 
-let token = {}
+let serverToken = {}
 
 server.on('request', function(req, res) {
-  console.log('req.url', req.url);
-
   if(req.method == 'POST') {
-    let jsonString = '';
+    let body = '';
     req.on('data', function (data) {
-        jsonString += data;
+        body += data;
     });
     req.on('end', function () {
-      const reqData = JSON.parse(jsonString);
-      console.log('reqData', reqData);
-      const text = reqData.text;
-      const id = reqData.id;
-      const dataToken = reqData.token;
-      if (reqData && text && id && dataToken && token.unigue && token.unigue === dataToken) {
-        if (token.date && token.date > Date.now()) {
+      const reqData = JSON.parse(body);
+
+      const editedOuterHTML = reqData.editedOuterHTML;
+      const editedBlockId = reqData.id;
+      const clientTokenUnique = reqData.token;
+      // если пришол измененный HTML + id + токен клиента равен текущему токену Node
+      if (reqData && editedOuterHTML && editedBlockId && clientTokenUnique && serverToken.unigue && serverToken.unigue === clientTokenUnique) {
+        // если время жызни токена больще текущей даты
+        if (serverToken.date && serverToken.date > Date.now()) {
           fs.readFile("index.html", "utf8", function(error,data){
-            change = data.replace(getRegExp(id), text);
+            change = data.replace(getRegExp(editedBlockId), editedOuterHTML);
             fs.writeFile("index.html", change, function(error){ 
               res.writeHead(200, {"Content-Type": "application/json"});
-              console.log('writeFile - ok');
             });        
           });
           res.end();
         } else {
-          console.log('writeFile - ne ok');
+          // если время жызни токена закончилось - розлогинюем пользователя удаляя его куку токена
           res.writeHead(400, {
             "Content-Type": "application/json",
             'Set-Cookie': 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;',
@@ -45,20 +44,23 @@ server.on('request', function(req, res) {
         }
       }
       
-      if (reqData && reqData.userName && reqData.userPassword) {
-        if (reqData.userName === userName && reqData.userPassword === userPassword) {
-          console.log('200');
-          res.writeHead(200, {"Content-Type": "application/json"});
-          token = {
+      const clientUserName = reqData.userName;
+      const clientUserPassword = reqData.userPassword;
+      if (reqData && clientUserName && clientUserPassword) {
+        // если от клиента пришли правильные логин и пароль 
+        if (clientUserName === defaultUserName && clientUserPassword === defaultUserPassword) {
+          // делаем токен с времям жызни 1 день
+          serverToken = {
             unigue: crypto.randomBytes(16).toString('hex'),
             date: Date.now() + 86400000,
           }
+          // передаем токен клиенту
           const json = JSON.stringify({ 
-            token: token.unigue, 
+            token: serverToken.unigue, 
           });
+          res.writeHead(200, {"Content-Type": "application/json"});
           res.end(json);
         } else {
-          console.log('400');
           res.writeHead(400, {
             "Content-Type": "application/json",
           });
@@ -67,11 +69,11 @@ server.on('request', function(req, res) {
       }
     });
   } else {
+    // если нет POST запросов читаем index.html и отображаем
     fs.readFile('index.html',function (err, data){
       res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
       res.write(data);
-      res.end();
-      console.log("get - index.html");        
+      res.end();      
     });
   }
 });
